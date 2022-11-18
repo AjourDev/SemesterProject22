176f7859074d99b1bc9c742f8c44d745f6cb3c45
Nick <nick@Nicks-MacBook-Pro.local>
2022-11-19 00:06:23 +0100

    Init 2

---
+++ Semesterprojekt/BotNotifier/BotNotifier/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using BotNotifier.ViewModels;
+using BotNotifier.Views;
+using Xamarin.Forms;
+
+namespace BotNotifier
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
---
+++ Semesterprojekt/BotNotifier/BotNotifier/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BotNotifier.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BotNotifier.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
+
